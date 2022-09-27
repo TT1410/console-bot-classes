b24ce0615a1807661ca9d0c53929adf676989222
TT1410 <112493080+TT1410@users.noreply.github.com>
2022-09-27 11:03:36 +0300

    Update record.py

--- console_bot/services/types/record.py
+++ console_bot/services/types/record.py
@@ -6,3 +6,9 @@ class Record:
     def __init__(self, name: str, phone: str = None) -> None:
         self.name: Name = Name(name)
         self.phones: list[Phone] = [Phone(phone)] if phone else []
+
+    def add_phone(self, phone: str) -> None:
+        phone = Phone(phone)
+
+        if phone not in self.phones:
+            self.phones.append(phone)
